c952557a8e4486fa782f79656fcde2d0c92fc333
Mh Limon <mhlimon571@gmail.com>
2022-11-20 01:47:14 +0600

    fourth commit

--- Treasure_map_identify.py
+++ Treasure_map_identify.py
@@ -1,3 +1,4 @@
+print("\n_____Here is yor 4 * 4 matrix._____\n")
 row1 = ["⬜️","⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️","⬜️"]
@@ -14,5 +15,5 @@ colum = int(position[1])
 selected_row = map[row-1]
 selected_row[colum - 1] = "X"
 
-print("_____Here is your new matix with your entered location_____")
+print("\n_____Here is your new matix with your entered location._____\n")
 print(f"{row1}\n{row2}\n{row3}\n{row4}")
